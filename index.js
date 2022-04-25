a03f3e362507f2d6636748722b8d46e502d14206
OndigoWendy <ondigowendy004@gmail.com>
2022-04-25 14:38:34 +0300

    Updates

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-// var person = 'wendy';
-// var array = [one, two];
-
-// function print() {
-//     console.log(myArray);
-// }
-// print();
-let y = document.getElementsByClassName('sampleClass');
-y[0].innerHTML = 'abc'
+let y = document.getElementsByClassName(`sampleClass`);
+y[0].innerHTML = `abc`
 
 let arr = ["nairobi", "kisumu", "mombasa"];
 for (let index = 0; index < arr.length; index++) {
@@ -17,6 +10,19 @@ for (let index = 0; index < arr.length; index++) {
 console.log("=========");
 arr.forEach(
     element => {
-        console.log('element ${element}');
+        console.log(`element ${element}`);
     }
 )
+
+
+const cars = ["Saab", "Volvo", "BMW"];
+const uiListElement = document.getElementById("car_types");
+for (let carItem = 0; carItem < cars.length; carItem++) {
+    const element = cars[carItem];
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(element);
+    node.appendChild(textnode);
+    uiListElement.appendChild(node);
+}
+
+// carsList();
